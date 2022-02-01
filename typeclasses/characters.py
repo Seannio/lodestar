457bb89a295a95ac964ec3f5d02d369d9f5e3f07
Seannio <Sean_E_M@icloud.com>
2022-02-01 14:33:51 +0900

    Stat system intro

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -32,3 +32,24 @@ class Character(DefaultCharacter):
     """
 
     pass
+
+class Character(DefaultCharacter):
+    # [...]
+    def at_object_creation(self):
+        """
+        Called only at initial creation. This is a rather silly
+        example since ability scores should vary from Character to
+        Character and is usually set during some character
+        generation step instead.
+        """
+        #set persistent attributes
+        self.db.strength = 5
+        self.db.agility = 4
+        self.db.magic = 2
+
+    def get_abilities(self):
+        """
+        Simple access method to return ability
+        scores as a tuple (str,agi,mag)
+        """
+        return self.db.strength, self.db.agility, self.db.magic
