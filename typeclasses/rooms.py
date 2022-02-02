73fbe8d643cff6a02823b3d725b9af0593843a7e
Seannio <Sean_E_M@icloud.com>
2022-02-02 14:11:07 +0900

    Programming is satisfying and relaxing

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -24,31 +24,28 @@ class Room(DefaultRoom):
 
     pass
 
-class TickerRoom(DefaultRoom):
-    "This room is ticked at regular intervals"
 
-    def at_object_creation(self):
-        super().at_object_creation()
-        "called only when the object is first created"
-        TICKER_HANDLER.add(10, self.at_ticker_update)
-        print("AAAAAAA. CREATED THING with TICKER!")
-
-    def at_ticker_update(self, *args, **kwargs):
-
-        randomgoods = ["reactor-plates.",
+randomgoods = ["reactor-plates.",
                 "high-speed graphene batteries.",
                 "dogs wearing hats.",
                 "weapons-grade chili.",
                 "illegally-sourced tubers." ]
 
-        ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + random.choice(randomgoods),
+ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + random.choice(randomgoods),
                  "A trickle of oil leaking from the ceiling-pipes splatters onto the ground nearby.",
                  "A deep, mechanical groan echoes down the length of the ship, sending shivers up the spine.",
                  "A mechanized loader rolls by on worn-down treads, hefting a pile of " + random.choice(randomgoods),
                  "Farther down the hangar, the sound of a dropped tool echoes dully against the high, metal walls." ]
-       
-        print(random.choice(ECHOES))
 
-        "ticked at regular intervals"
+class TickerRoom(DefaultRoom):
+    "This room is ticked at regular intervals"
+
+    def at_object_creation(self):
+        super().at_object_creation()
+        "60 seconnd interval"
+        TICKER_HANDLER.add(60, self.at_ticker_update)
+        print("AAAAAAA. CREATED THING with TICKER!")
+
+    def at_ticker_update(self, *args, **kwargs):
         print("This is a regular ticker update.")
         self.msg_contents("|w%s|n" % random.choice(ECHOES))
