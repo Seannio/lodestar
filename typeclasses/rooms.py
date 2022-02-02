1d8e8309bfd97a4236668e79df2d07edf4d4cbae
Seannio <Sean_E_M@icloud.com>
2022-02-02 14:14:18 +0900

    Programming is satisfying and relaxing

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -29,13 +29,17 @@ randomgoods = ["reactor-plates.",
                 "high-speed graphene batteries.",
                 "dogs wearing hats.",
                 "weapons-grade chili.",
-                "illegally-sourced tubers." ]
+                "illegally-sourced tubers.",
+                "curiously-made octagons",
+                "thousands of individually-wrapped screws and bolts",
+                "backalley space torpedoes" ]
 
 ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + random.choice(randomgoods),
                  "A trickle of oil leaking from the ceiling-pipes splatters onto the ground nearby.",
                  "A deep, mechanical groan echoes down the length of the ship, sending shivers up the spine.",
                  "A mechanized loader rolls by on worn-down treads, hefting a pile of " + random.choice(randomgoods),
-                 "Farther down the hangar, the sound of a dropped tool echoes dully against the high, metal walls." ]
+                 "Farther down the hangar, the sound of a dropped tool echoes dully against the high, metal walls.",
+                 "In the distance, a stacking-machine struggles to find the right place for a load of" + random.choice(randomgoods)]
 
 class TickerRoom(DefaultRoom):
     "This room is ticked at regular intervals"
