6a027cc0797866b9b07e06c451c6b9d05514a6aa
Seannio <Sean_E_M@icloud.com>
2022-02-02 15:30:07 +0900

    adding config for strings

---
+++ config/configlists.py
@@ -0,0 +1,21 @@
+
+# These are ambience strings for indoor rooms in the Dirge sector
+DIRGE_INDOOR_AMBIENCE_STRINGS = (
+    "Off in the distance, the sirens of a private police-force wail, rising and fading.",
+    "The whine of an electric tool sounds from somewhere nearby, then the crunch of cutting metal.",
+    "For a moment, you catch the smell of distant street-grills, thick and oily.",
+    "The loud revs of aftermarket engines fill the air, echoed down concrete streets.",
+    "An electric hum picks up, reaching a distinctly noticeable level before fading back to nothing.",
+    "You catch the acrid whiff of burning electronics, filtered in from somewhere else.",
+    "Gunshots sound off in the distance. Just a few, then relative silence.",
+    "Nearby, muffled shouting marks the start of a fight only a wall or two away.",
+    "The sound of crunching, gurgling pipes interrupt the background noise here, but only for a few moments.",
+)
+
+SCREENOBJECT_STRINGS = (
+                "\nOn the tiny holopanel: An ad for Astro Lite, the universe's favorite lager brewed in space!",
+                "\nOn the tiny holopanel: A technicolor ad for the wonders of flesh-eating nanites. ",
+                "\nOn the tiny holopanel: A dead rat spinning perpetually.",
+                "\nOn the tiny holopanel: A hat with fuzzy legs dancing to some out-of-date polka.",
+                "\nOn the tiny holopanel: A static picture of a frog with six eyes, imploring you to invest in a company named TOADLICK." 
+)
