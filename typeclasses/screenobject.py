1bfad3375cd11655e8a873733db3f11815d6ced8
Seannio <Sean_E_M@icloud.com>
2022-02-02 09:17:39 +0900

    random functions for rooms and objects

--- typeclasses/screenobject.py
+++ typeclasses/screenobject.py
@@ -9,9 +9,11 @@ class ScreenObject(Object):
     def at_object_creation(self):
         """Called when object is first created"""
         self.db.tv_texts = \
-               ["On the tiny holopanel: An ad for Astro Lite, the universe's favorite lager brewed in space!",
-                "On the tiny holopanel: A technicolor ad for the wonders of flesh-eating nanites. ",
-                "On the tiny holopanel: a dead rat spinning perpetually." ]
+               ["\nOn the tiny holopanel: An ad for Astro Lite, the universe's favorite lager brewed in space!",
+                "\nOn the tiny holopanel: A technicolor ad for the wonders of flesh-eating nanites. ",
+                "\nOn the tiny holopanel: A dead rat spinning perpetually.",
+                "\nOn the tiny holopanel: A hat with fuzzy legs dancing to some out-of-date polka.",
+                "\nOn the tiny holopanel: A static picture of a frog with six eyes, imploring you to invest in a company named TOADLICK." ]
 
     def return_appearance(self, looker):
         """
