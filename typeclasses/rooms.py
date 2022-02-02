264b9abf686539d7c0f3617278915d705340f3c0
Seannio <Sean_E_M@icloud.com>
2022-02-02 10:56:30 +0900

    bug

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -8,19 +8,6 @@ import random
 from evennia import DefaultRoom, TICKER_HANDLER
 from random import choice
 
-randomgoods = \
-               ["reactor-plates.",
-                "high-speed graphene batteries.",
-                "dogs wearing hats.",
-                "weapons-grade chili.",
-                "illegally-sourced tubers." ]
-    
-ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + choice(randomgoods),
-          "A trickle of oil leaking from the ceiling-pipes splatters onto the ground nearby.",
-          "A deep, mechanical groan echoes down the length of the ship, sending shivers up the spine.",
-          "A mechanized loader rolls by on worn-down treads, hefting a pile of " + choice(randomgoods),
-          "Farther down the hangar, the sound of a dropped tool echoes dully against the high, metal walls."]
-
 class Room(DefaultRoom):
     """
     Rooms are like any Object, except their location is None
@@ -36,10 +23,23 @@ class Room(DefaultRoom):
 
 class TickerRoom(DefaultRoom):
     "This room is ticked at regular intervals"
+
+    randomgoods = ["reactor-plates.",
+                "high-speed graphene batteries.",
+                "dogs wearing hats.",
+                "weapons-grade chili.",
+                "illegally-sourced tubers." ]
+    
+    ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + choice(randomgoods),
+                 "A trickle of oil leaking from the ceiling-pipes splatters onto the ground nearby.",
+                 "A deep, mechanical groan echoes down the length of the ship, sending shivers up the spine.",
+                 "A mechanized loader rolls by on worn-down treads, hefting a pile of " + choice(randomgoods),
+                 "Farther down the hangar, the sound of a dropped tool echoes dully against the high, metal walls."]
+
        
     def at_object_creation(self):
         "called only when the object is first created"
-        TICKER_HANDLER.add(60 * 60, self.at_weather_update)
+        TICKER_HANDLER.add(10, self.at_weather_update)
 
     def at_weather_update(self, *args, **kwargs):
         "ticked at regular intervals"
