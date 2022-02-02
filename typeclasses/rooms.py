92bfceb7b00aaa0ea6f4ba6b9ba03ae113fbc61b
Seannio <Sean_E_M@icloud.com>
2022-02-02 13:30:14 +0900

    reorganized arrays so the ECHOES call works with the ticker.

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -24,21 +24,21 @@ class Room(DefaultRoom):
 
     pass
 
-class TickerRoom(DefaultRoom):
-    "This room is ticked at regular intervals"
-
     randomgoods = ["reactor-plates.",
                 "high-speed graphene batteries.",
                 "dogs wearing hats.",
                 "weapons-grade chili.",
                 "illegally-sourced tubers." ]
     
-    self.ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + choice(randomgoods),
+    ECHOES = ["A forklift trundles by, carrying a huge pallet of  " + choice(randomgoods),
                  "A trickle of oil leaking from the ceiling-pipes splatters onto the ground nearby.",
                  "A deep, mechanical groan echoes down the length of the ship, sending shivers up the spine.",
                  "A mechanized loader rolls by on worn-down treads, hefting a pile of " + choice(randomgoods),
                  "Farther down the hangar, the sound of a dropped tool echoes dully against the high, metal walls."]
        
+class TickerRoom(DefaultRoom):
+    "This room is ticked at regular intervals"
+
     def at_object_creation(self):
         "called only when the object is first created"
         TICKER_HANDLER.add(10, self.at_ticker_update)
