903f3a2d4871206fafc0489585335bda4f96292d
Seannio <Sean_E_M@icloud.com>
2022-02-22 10:45:59 +0900

    bugfix

--- typeclasses/furniture.py
+++ typeclasses/furniture.py
@@ -38,13 +38,17 @@ class SittableOb(DefaultObject):
             stander (Object): The one trying to stand up.
         """
         current = self.db.sitting
-        if not stander == current:
-            stander.msg("You are not sitting on %s." % self.key)
-        else:
-            self.db.sitting = None
+        try:
+            if not stander == current:
+                stander.msg("You are not sitting on %s." % self.key)
+            else:
+                self.db.sitting = None
+                stander.db.is_sitting = False
+                stander.msg(self.db.messages['stand_msg'])
+                #stander.msg(f"You stand up from {self.key}")
+        except AttributeError:
+            stander.msg("You aren't sitting on anything.")
             stander.db.is_sitting = False
-            stander.msg(self.db.messages['stand_msg'])
-            #stander.msg(f"You stand up from {self.key}")
 
 
 class CmdSit(Command):
