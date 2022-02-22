ce686dbd4455e3a1c68f562bb6e2b4f2125a3ba9
Seannio <Sean_E_M@icloud.com>
2022-02-22 13:57:16 +0900

    adding additional condition check before sitting to prevent exception

--- typeclasses/furniture.py
+++ typeclasses/furniture.py
@@ -26,15 +26,18 @@ class SittableOb(DefaultObject):
         if current:
             if sitter in current:
                 sitter.msg("You are already sitting on %s." % self.key)
+                return
             elif len(self.db.sitting) >= self.db.space:
                 sitter.msg( "There's no space left on %s" % self.key)
                 return
-            elif len(self.db.sitting) <= self.db.space:
-                self.db.sitting.append(sitter)
-                sitter.db.is_sitting= True
+                print("NOW.... LEN of the SITTING ARRAY: %i" % len(self.db.sitting))
+                print(" VERSUS LEN of the sitting space: %i" % self.db.space)
+            
+            self.db.sitting.append(sitter)
+            sitter.db.is_sitting= True
 
-                sitter.msg(self.db.messages['sit_msg'])
-                sitter.location.msg_contents(self.db.messages['osit_msg'], exclude=sitter)
+            sitter.msg(self.db.messages['sit_msg'])
+            sitter.location.msg_contents(self.db.messages['osit_msg'], exclude=sitter)
 
     def do_stand(self, stander):
         """
